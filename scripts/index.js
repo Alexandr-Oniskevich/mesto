e0442d471c6499470271073a95833f3b6973fa75
Alexandr Oniskevich <alexandroniskevich93@gmail.com>
2022-10-03 20:17:24 +0300

    new function card

--- scripts/index.js
+++ scripts/index.js
@@ -25,30 +25,67 @@ const initialCards = [
   }
 ]; 
 
-//Функция добавления карточек
-const addCards = initialCards.forEach(el=>{
-  // Получаем контейнер карточек
-  const cardContainer = document.querySelector('.elements__list');
-  const cardTemplate = document.querySelector('#card-template').content;
-  const cardElement = cardTemplate.querySelector('.elements__card').cloneNode(true);
-  const deleteButton = cardElement.querySelector('.elements__del-card');
-    
-  cardElement.querySelector('.elements__text').textContent = el.name;
-  cardElement.querySelector('.elements__img').src = el.link;
-  //добавляем карточки
-  cardContainer.append(cardElement);
-  //ставим лайк
-  cardElement.querySelector('.elements__like').addEventListener('click', function (evt) {
-    evt.target.classList.toggle('elements__like_active');
+// Получаем контейнер карточек
+const cardContainer = document.querySelector('.elements__list');
+// Получаем template
+const cardTemplate = document.querySelector('#card-template');
+//Функции добавления карточек из массива
+const render = () => {
+  initialCards.forEach(el=>{
+    const createCard = createCardNode(el.name, el.link);
+    cardContainer.append(createCard);
   });
 
-  //Удаление карточек
-  deleteButton.addEventListener('click', function () {
-    const listItem = deleteButton.closest('.elements__card');
-    listItem.remove();
-  }); 
-});
+  btnSubmit.addEventListener('click', handleAddCard);
+}
 
+const createCardNode = (name, link) => {
+  const createCard = cardTemplate.content.cloneNode(true);
+  const cardName = createCard.querySelector('.elements__text');
+  const cardPic = createCard.querySelector('.elements__img');
+
+  cardName.textContent = name;
+  cardPic.src = link;
+
+  const deleteBtn = createCard.querySelector('.elements__del-card');
+  deleteBtn.addEventListener('click', handleDeleteCard);
+
+  const likeBtn = createCard.querySelector('.elements__like');
+  likeBtn.addEventListener('click', handlLikeCard);
+  
+  return createCard;
+}
+
+//Функция добавления карточек через форму
+
+// Получаем popup формы добавления карточки
+const popupCards = document.querySelector('#popup-cards');
+//Получаем кнопку добавления карточки
+const btnCardAdd = document.querySelector('.profile__add-btn');
+const inputNameCard = popupCards.querySelector('.popup__input_item_name');
+const inputLinkCard = popupCards.querySelector('.popup__input_item_description');
+const btnSubmit = popupCards.querySelector('.popup__btn-submit');
+
+const handleAddCard =(evt) => {
+  evt.preventDefault(); 
+  const card = createCardNode(inputNameCard.value, inputLinkCard.value);
+  cardContainer.prepend(card);
+  inputNameCard.value = '';
+  closePopup();
+}
+
+//функция удаления карточки
+const handleDeleteCard = (e) => {
+  const currentEl = e.target.closest('.elements__card');
+  currentEl.remove();
+}
+
+//функция лайка
+const handlLikeCard = (e) => {
+  e.target.classList.toggle('elements__like_active');
+}
+
+render();
 
 //Получаем кнопку редактирования профиля
 let buttonEdit = document.querySelector('.profile__btn-edit');
@@ -66,10 +103,6 @@ const profileJob = profile.querySelector('.profile__profession');
 const inputName = popupProfile.querySelector('.popup__input_item_name');
 // Получаем поле ввода профессии
 const inputJob = popupProfile.querySelector('.popup__input_item_description');
-//Получаем кнопку добавления карточки
-const btnCardAdd = document.querySelector('.profile__add-btn');
-// Получаем popup формы добавления карточки
-const popupCards = document.querySelector('#popup-cards');
 //Кнопка закрытия попапа карточки
 const cardButtonClose = popupCards.querySelector('.popup__btn-close');
 
@@ -96,6 +129,7 @@ buttonEdit.addEventListener("click", openPopup);
 
 function closePopup() {
   popupProfile.classList.remove('popup_open');
+  popupCards.classList.remove('popup_open');
 }
 
 buttonClose.addEventListener("click", closePopup);
